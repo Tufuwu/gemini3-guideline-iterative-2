586c9ad31e2c71c891c85b6e6021a2015d777c86
wu <wu@.com>
2026-01-30 10:56:47 +0800

    Logs-kolotaev/vakt

--- setup.py
+++ setup.py
@@ -1,55 +1,72 @@
 from setuptools import setup, find_packages
-import os
+from os import path
 
-here = os.path.abspath(os.path.dirname(__file__))
 
-def read_file(filename):
-    path = os.path.join(here, filename)
-    return open(path, encoding="utf-8").read() if os.path.exists(path) else ""
+here = path.abspath(path.dirname(__file__))
+about = {}
+with open(path.join(here, 'vakt', 'version.py'), mode='r', encoding='utf-8') as f:
+    exec(f.read(), about)
 
-README = read_file("README.rst")
-NEWS = read_file("NEWS.txt")
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
-version = "0.5.0"
 
-install_requires = [
-    "urllib3>=1.7.1",
-    "dnspython>=1.13.0",
-]
-
-extras_require = {
-    "test": [
-        "pytest",
-        "pytest-mock",
-        "pyOpenSSL>=0.14",
-    ]
-}
-
-setup(
-    name="python-etcd",
-    version=version,
-    description="A python client for etcd",
-    long_description=README + "\n\n" + NEWS,
-    long_description_content_type="text/x-rst",
-    classifiers=[
-        "Topic :: System :: Distributed Computing",
-        "Topic :: Software Development :: Libraries",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3 :: Only",
-        "Topic :: Database :: Front-Ends",
-    ],
-    keywords="etcd raft distributed log api client",
-    author="Jose Plana",
-    author_email="jplana@gmail.com",
-    url="http://github.com/jplana/python-etcd",
-    license="MIT",
-    packages=find_packages("src"),
-    package_dir={"": "src"},
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=install_requires,
-
-    # Modern testing: no nose, no test_suite
-    extras_require=extras_require,
-)
+if __name__ == '__main__':
+    setup(
+        name='vakt',
+        description='Attribute-based access control (ABAC) SDK for Python',
+        keywords='ACL ABAC access-control policy security authorization permission',
+        version=about['__version__'],
+        author='Egor Kolotaev',
+        author_email='ekolotaev@gmail.com',
+        license="Apache 2.0 license",
+        url='http://github.com/kolotaev/vakt',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        py_modules=['vakt'],
+        python_requires='>=3.6',
+        install_requires=[
+            'jsonpickle>=2.0',
+        ],
+        extras_require={
+            'dev': [
+                'pytest~=7.0',
+                'pytest-cov~=4.0',
+                'pylint~=2.13',
+                'PyMySQL~=1.0',
+                'psycopg2cffi~=2.8',
+            ],
+            'mongo': [
+                'pymongo~=4.1',
+            ],
+            'sql': [
+                'SQLAlchemy~=1.4',
+            ],
+            'redis': [
+                'redis~=4.3'
+            ],
+        },
+        packages=find_packages(exclude='tests'),
+        classifiers=[
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: Apache Software License',
+            'Operating System :: OS Independent',
+            'Topic :: System :: Systems Administration',
+            'Topic :: System :: Networking',
+            'Topic :: System :: Networking :: Firewalls',
+            'Topic :: Security',
+            'Topic :: Software Development',
+            'Topic :: Utilities',
+            'Natural Language :: English',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
+            'Programming Language :: Python :: 3.11',
+            'Programming Language :: Python :: Implementation :: CPython',
+            'Programming Language :: Python :: Implementation :: PyPy',
+        ],
+    )
